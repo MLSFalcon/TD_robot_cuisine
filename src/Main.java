4a5cc62b692d782c0aa39542d90a6bd92ba9c6e1
Mateo LAPETRE <m.lapetre-sarthou@lprs.fr>
2025-01-09 16:40:57 +0100

    refactor!: fin du TD sans la partie complémentaire

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("saisir le nom de l'ingredient");
+        String nom = sc.nextLine();
+        System.out.println("Maintenant la quantité");
+        int quantite = sc.nextInt();
+        sc.skip("\n");
+        System.out.println("Pour finir l'unite");
+        String unite = sc.nextLine();
+        Ingredient monIngredient = new Ingredient(nom,quantite,unite);
+        System.out.println(monIngredient.afficher());
     }
 }
